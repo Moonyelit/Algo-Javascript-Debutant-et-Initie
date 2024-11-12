061e1d4ad1f26f3cde493a53825f9bb3ad124352
moonye@live.fr <moonye@live.fr>
2024-11-12 16:48:10 +0100

    ADD :EXO 1 et 2

---
+++ script.js
@@ -0,0 +1,15 @@
+// EXO 1
+console.log("Hello World");
+console.log("Bienvenue dans le monde de la programmation !");
+
+
+//EXO 2
+let nombre1 = 5;
+let nombre2 = 10;
+
+console.log(nombre1+nombre2)
+
+//EXO 3
+const tableFruts = ["pomme", "banane", "cerise"];
+for 
+
